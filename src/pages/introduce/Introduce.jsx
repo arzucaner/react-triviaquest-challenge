ded9169f7a5ece6000b2c46b2d4b98f2c1d3d143
Arzu Caner <arzuguneycaner@gmail.com>
2023-10-29 16:13:33 +0000

    initial commit

---
+++ src/pages/introduce/Introduce.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Introduce.css';
+
+const Introduce = () => {
+  return (
+    <div className='introduce'>
+        <div className="introduce-container">
+             <img src="/images/logo.png" alt="logo" />
+             
+        </div>
+    </div>
+  )
+}
+
+export default Introduce
