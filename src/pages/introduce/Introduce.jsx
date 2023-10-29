16d5a57b624e0dfb2cebc2f130b242932a7f076e
Arzu Caner <arzuguneycaner@gmail.com>
2023-10-29 18:51:44 +0000

    Update introduce.css, andintroduce.jsx and logo

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './Introduce.css';
+import Dropdown from '../../components/dropdown/Dropdown'
+import './Introduce.css'
 
 const Introduce = () => {
+    const difficulty = ["easy", "medium", "hard"]
   return (
     <div className='introduce'>
         <div className="introduce-container">
              <img src="/images/logo.png" alt="logo" />
+             <Dropdown/>
+             <div className='introduce-btn'> Start Quiz</div>
              
         </div>
     </div>
